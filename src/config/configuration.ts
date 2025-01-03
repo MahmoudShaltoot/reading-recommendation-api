817ea900473645f003cceee3e136f642007c5e6a
mahmoud.shaltoot <mahmoud.shaltoot@squadio.com>
2025-01-03 18:19:00 +0200

    define custom configration object

--- src/config/configuration.ts
+++ src/config/configuration.ts
@@ -1,8 +1,10 @@
 export default () => ({
-    port: parseInt(process.env.PORT, 10) || 3000,
-    database: {
-      host: process.env.DATABASE_HOST,
-      password: process.env.DATABASE_PASSWORD,
-      port: parseInt(process.env.DATABASE_PORT, 10) || 5432
+    PORT: parseInt(process.env.PORT, 10) || 3000,
+    DATABASE: {
+      HOST: process.env.DATABASE_HOST,
+      USERNAME: process.env.DATABASE_USERNAME,
+      PASSWORD: process.env.DATABASE_PASSWORD,
+      NAME: process.env.DATABASE_NAME,
+      PORT: parseInt(process.env.DATABASE_PORT, 10) || 5432
     }
   });
