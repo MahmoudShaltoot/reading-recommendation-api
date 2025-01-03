68f2d55928fd7cd37b734d54fedaa47269df131b
mahmoud.shaltoot <mahmoud.shaltoot@squadio.com>
2025-01-03 18:22:22 +0200

    create user entity

--- src/users/entities/user.entity.ts
+++ src/users/entities/user.entity.ts
@@ -1,1 +1,25 @@
-export class User {}
+import { Column, CreateDateColumn, Entity, PrimaryGeneratedColumn, UpdateDateColumn } from "typeorm";
+
+@Entity()
+export class User {
+    @PrimaryGeneratedColumn()
+    id: number;
+
+    @Column()
+    full_name: string;
+
+    @Column({unique: true})
+    username: string;
+
+    @Column()
+    password: string;
+
+    @Column({default: false})
+    is_admin: boolean
+
+    @CreateDateColumn()
+    created_at: Date;
+  
+    @UpdateDateColumn()
+    updated_at: Date;
+}
