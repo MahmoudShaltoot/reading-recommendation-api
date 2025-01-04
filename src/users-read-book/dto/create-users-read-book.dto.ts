9faea98524c6a26fb857ba7a1d03a098c3c3635a
mahmoud.shaltoot <mahmoud.shaltoot@squadio.com>
2025-01-04 17:26:04 +0200

    define users read books DTOs

--- src/users-read-book/dto/create-users-read-book.dto.ts
+++ src/users-read-book/dto/create-users-read-book.dto.ts
@@ -1,1 +1,23 @@
-export class CreateUsersReadBookDto {}
+import { IsNotEmpty, IsNumber, IsString, Max, Min } from "class-validator";
+
+export class CreateUsersReadBookDto {
+    @IsNumber()
+    @IsNotEmpty()
+    user_id: string;
+
+    @IsNumber()
+    @IsNotEmpty()
+    book_id: string;
+
+    @IsNumber()
+    @IsNotEmpty()
+    @Min(1)
+    @Max(100000)
+    start_page: string
+
+    @IsNumber()
+    @IsNotEmpty()
+    @Min(1)
+    @Max(100000)
+    end_page: string
+}
