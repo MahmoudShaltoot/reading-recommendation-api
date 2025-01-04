1a929d02343a1e0b80cdf1867cc427fa51f15247
mahmoud.shaltoot <mahmoud.shaltoot@squadio.com>
2025-01-04 15:26:29 +0200

    implement book create & update DTOs

--- src/book/dto/create-book.dto.ts
+++ src/book/dto/create-book.dto.ts
@@ -1,1 +1,15 @@
-export class CreateBookDto {}
+import { IsInt, IsNotEmpty, IsNumber, IsString, Max, MaxLength, MinLength } from "class-validator";
+
+export class CreateBookDto {
+    @IsString()
+    @IsNotEmpty()
+    @MinLength(1)
+    @MaxLength(500)
+    title: string;
+
+    @IsNumber()
+    @IsNotEmpty()
+    @IsInt()
+    @Max(10000)
+    num_of_pages: number;
+}
