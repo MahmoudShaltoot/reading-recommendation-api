426782ed16d21faf8c5d6bf56411bb310e3f85a6
mahmoud.shaltoot <mahmoud.shaltoot@squadio.com>
2025-01-05 17:58:11 +0200

    cache user read pages

--- src/event-handler/user-read-book-event-handler.ts/user-read-book.handler.ts
+++ src/event-handler/user-read-book-event-handler.ts/user-read-book.handler.ts
@@ -1,11 +1,18 @@
 import { Injectable } from "@nestjs/common";
 import { OnEvent } from "@nestjs/event-emitter";
+import { RedisService } from "src/cache/redis.service";
 import { UserReadBook } from "src/users-read-book/entities/user-read-book.entity";
 
 @Injectable()
 export class UserReadBookEventHandler {
+	constructor(private readonly redisService: RedisService) {}
+
 	@OnEvent('USER_READ_BOOK')
-	handleUserReadBookEvent(event: UserReadBook) {
-		console.log(event);
+	async handleUserReadBookEvent(event: UserReadBook) {
+		const readPages: number[] = [];
+		for (let pageIndex = event.start_page; pageIndex <= event.end_page; pageIndex++) {
+			readPages.push(pageIndex)
+		}
+		this.redisService.addToSet(event.book.id.toString(), readPages)
 	}
 }
